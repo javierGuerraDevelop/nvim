ce24d3f298bce1b97f158a742c8c7cc35ef96b52
Javier Guerra <133339273+javierGuerraDevelop@users.noreply.github.com>
2024-10-04 08:20:12 -0400

    Update theme.lua

--- lua/chalupa/theme.lua
+++ lua/chalupa/theme.lua
@@ -1,9 +1,8 @@
+-- Transparency in case it is not supported by my theme
+vim.cmd('au ColorScheme * hi Normal guibg=NONE')     -- transparency
+vim.cmd('au ColorScheme * hi SignColumn guibg=NONE') -- transparency
+
 require('github-theme').setup({
   terminal_colors = true,
 })
 vim.cmd('colorscheme github_dark')
-
-
--- Transparency in case it is not supported by my theme
-vim.cmd('au ColorScheme * hi Normal guibg=NONE')     -- transparency
-vim.cmd('au ColorScheme * hi SignColumn guibg=NONE') -- transparency
