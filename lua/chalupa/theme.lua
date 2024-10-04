e80ee124455e3bc03ed9873a360de2deed21b6b8
Javier Guerra <133339273+javierGuerraDevelop@users.noreply.github.com>
2024-10-04 07:56:34 -0400

    Update theme.lua

--- lua/chalupa/theme.lua
+++ lua/chalupa/theme.lua
@@ -1,23 +1,9 @@
-require("catppuccin").setup({
-  flavour = "mocha", -- You can choose 'latte', 'frappe', 'macchiato', 'mocha'
-  no_italic = true,
-  transparent_background = true,
-  integrations = {
-    cmp = true,
-    gitsigns = true,
-    nvimtree = true,
-    treesitter = true,
-    notify = false,
-    mini = {
-      enabled = true,
-      indentscope_color = ""
-    }
-    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-  },
-  color_overrides = {
-    mocha = {
-      base = "#0F0F0F", -- Background color
-    }
-  }
+require('github-theme').setup({
+  terminal_colors = true,
 })
-vim.cmd("colorscheme catppuccin")
+vim.cmd('colorscheme github_dark')
+
+
+-- Transparency in case it is not supported by my theme
+vim.cmd('au ColorScheme * hi Normal guibg=NONE')     -- transparency
+vim.cmd('au ColorScheme * hi SignColumn guibg=NONE') -- transparency
