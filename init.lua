1b983e0796528625e2eb926caf3a6117f257e993
Javier Guerra <javierguerradevelop@gmail.com>
2024-10-16 02:07:19 -0400

    removed redundancy

--- init.lua
+++ init.lua
@@ -106,7 +106,7 @@ require('lazy').setup({
         end
     },
     { -- LSP
-        'neovim/nvim-lspconfig', 'hrsh7th/nvim-cmp', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path',
+        'hrsh7th/nvim-cmp', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path',
         'hrsh7th/cmp-cmdline', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', 'rafamadriz/friendly-snippets' },
     {
         'neovim/nvim-lspconfig',
