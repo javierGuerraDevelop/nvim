4329b856659980ed2b2b0ea8a37ff275bf7824aa
Javier Guerra <javierguerradevelop@gmail.com>
2024-11-28 03:08:50 -0500

    update config

--- init.lua
+++ init.lua
@@ -1,42 +1,29 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system { 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable',
-        lazypath }
+   vim.fn.system { 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath }
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Combine all plugin specifications
-local plugins = {}
-for _, module in ipairs({
-    'theme',
-    'telescope',
-    'treesitter',
-    'lualine',
-    'mason',
-    'autocomplete',
-    'gitsigns',
-    'autoclose',
-    'harpoon',
-    'tailwind',
-    'null-ls',
-    'comment',
-}) do
-    local success, module_plugins = pcall(require, 'chalupa.' .. module)
-    if success then
-        for _, plugin in ipairs(module_plugins) do
-            table.insert(plugins, plugin)
-        end
-    end
-end
+local plugins = {
+   require('chalupa.theme'),
+   require('chalupa.telescope'),
+   require('chalupa.treesitter'),
+   require('chalupa.lualine'),
+   require('chalupa.mason'),
+   require('chalupa.autocomplete'),
+   require('chalupa.gitsigns'),
+   require('chalupa.autoclose'),
+   require('chalupa.harpoon'),
+   require('chalupa.tailwind'),
+   require('chalupa.null-ls'),
+   require('chalupa.comment')
+}
 
 require('lazy').setup(plugins)
 
--- Setups
+-- Additional setups
 require('neodev').setup()
-require('autoclose').setup()
 
-require('chalupa.treesitter')
-require('chalupa.mason')
-require('chalupa.autocomplete')
+-- Require other configurations
 require('chalupa.ui')
 require('chalupa.remaps')
