dff64a908e4062399a9f5158e7a52682ae5bea29
javier guerra <javierguerradevelop@gmail.com>
2024-12-19 14:49:15 -0500

    removed rose pine comments

--- lua/chalupa/theme.lua
+++ lua/chalupa/theme.lua
@@ -2,66 +2,11 @@ return {
     {
         'projekt0n/github-nvim-theme',
         name = 'github-theme',
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
+        lazy = false,
+        priority = 1000,
         config = function()
-            require('github-theme').setup({
-                -- ...
-            })
-
-            vim.cmd('colorscheme github_dark_default')
+            require('github-theme').setup()
+            vim.cmd('colorscheme github_dark_high_contrast')
         end,
     }
-    -- {
-    --     'rose-pine/neovim',
-    --     name = 'rose-pine',
-    --     lazy = false,
-    --     priority = 1000,
-    --     config = function()
-    --         require("rose-pine").setup({
-    --             variant = "auto",
-    --             dark_variant = "main",
-    --             dim_inactive_windows = false,
-    --             extend_background_behind_borders = true,
-    --             enable = {
-    --                 terminal = true,
-    --                 legacy_highlights = true,
-    --                 migrations = true,
-    --             },
-    --             styles = {
-    --                 bold = true,
-    --                 italic = false,
-    --                 transparency = true,
-    --             },
-    --             groups = {
-    --                 border = "muted",
-    --                 link = "iris",
-    --                 panel = "surface",
-    --                 error = "love",
-    --                 hint = "iris",
-    --                 info = "foam",
-    --                 note = "pine",
-    --                 todo = "rose",
-    --                 warn = "gold",
-    --                 git_add = "foam",
-    --                 git_change = "rose",
-    --                 git_delete = "love",
-    --                 git_dirty = "rose",
-    --                 git_ignore = "muted",
-    --                 git_merge = "iris",
-    --                 git_rename = "pine",
-    --                 git_stage = "iris",
-    --                 git_text = "rose",
-    --                 git_untracked = "subtle",
-    --                 h1 = "iris",
-    --                 h2 = "foam",
-    --                 h3 = "rose",
-    --                 h4 = "gold",
-    --                 h5 = "pine",
-    --                 h6 = "foam",
-    --             },
-    --         })
-    --         vim.cmd("colorscheme rose-pine")
-    --     end,
-    -- }
 }
