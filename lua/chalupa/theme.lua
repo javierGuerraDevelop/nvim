783109cdfb56729aff3f373f60f6a88de08bb49f
javier guerra <javierguerradevelop@gmail.com>
2024-12-26 19:54:50 -0500

    return back to rose pine

--- lua/chalupa/theme.lua
+++ lua/chalupa/theme.lua
@@ -1,12 +1,54 @@
 return {
     {
-        'projekt0n/github-nvim-theme',
-        name = 'github-theme',
+        'rose-pine/neovim',
+        name = 'rose-pine',
         lazy = false,
         priority = 1000,
         config = function()
-            require('github-theme').setup()
-            vim.cmd('colorscheme github_dark_high_contrast')
+            require("rose-pine").setup({
+                variant = "auto",
+                dark_variant = "main",
+                dim_inactive_windows = false,
+                extend_background_behind_borders = true,
+                enable = {
+                    terminal = true,
+                    legacy_highlights = true,
+                    migrations = true,
+                },
+                styles = {
+                    bold = true,
+                    italic = false,
+                    transparency = true,
+                },
+                groups = {
+                    border = "muted",
+                    link = "iris",
+                    panel = "surface",
+                    error = "love",
+                    hint = "iris",
+                    info = "foam",
+                    note = "pine",
+                    todo = "rose",
+                    warn = "gold",
+                    git_add = "foam",
+                    git_change = "rose",
+                    git_delete = "love",
+                    git_dirty = "rose",
+                    git_ignore = "muted",
+                    git_merge = "iris",
+                    git_rename = "pine",
+                    git_stage = "iris",
+                    git_text = "rose",
+                    git_untracked = "subtle",
+                    h1 = "iris",
+                    h2 = "foam",
+                    h3 = "rose",
+                    h4 = "gold",
+                    h5 = "pine",
+                    h6 = "foam",
+                },
+            })
+            vim.cmd("colorscheme rose-pine")
         end,
     }
 }
