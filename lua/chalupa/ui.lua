820c48e20cda1e92bf71c04c186d79e78a6ae856
javier guerra <javierguerradevelop@gmail.com>
2025-12-02 15:25:58 -0500

    update back to 4

--- lua/chalupa/ui.lua
+++ lua/chalupa/ui.lua
@@ -4,9 +4,9 @@ vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#FF69B4' }) -- Current line numbe
 vim.opt.colorcolumn = "120"
 vim.wo.relativenumber = true                               -- Relative line numbers at the left
 vim.opt.termguicolors = true                               -- Nicer colors
-vim.opt.tabstop = 8                                        -- Tab width is 8 spaces
-vim.opt.softtabstop = 8                                    -- Tab width is 8 spaces
-vim.opt.shiftwidth = 8                                     -- Indent amount width > in visual mode
+vim.opt.tabstop = 4                                        -- Tab width is 4 spaces
+vim.opt.softtabstop = 4                                    -- Tab width is 4 spaces
+vim.opt.shiftwidth = 4                                     -- Indent amount width > in visual mode
 vim.opt.expandtab = true                                   -- Spaces instead of tabs
 vim.g.codeium_enabled = false                              -- codeium disabled
 vim.g.codeium_disable_bindings = 1                         -- codeium bindings disabled
@@ -35,10 +35,10 @@ vim.api.nvim_create_autocmd("TextYankPost", { -- Highlight on yank
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "rust",
-  callback = function()
-    vim.opt_local.tabstop = 8
-    vim.opt_local.shiftwidth = 8
-    vim.opt_local.softtabstop = 8
-  end,
+        pattern = "rust",
+        callback = function()
+                vim.opt_local.tabstop = 4
+                vim.opt_local.shiftwidth = 4
+                vim.opt_local.softtabstop = 4
+        end,
 })
